b3be57ce20b42c692b4b9aae735bd0473d71cc8f
Neftaly <neftalyrv23@gmail.com>
2025-03-15 21:53:10 -0400

    Set up express and users and products routers. Added gitignore file

---
+++ index.js
@@ -0,0 +1,15 @@
+import   express  from "express"
+import { ProductsRouter, UsersRouter } from "./routers/index.js"
+const app = express()
+const port = 3000
+
+app.use('/products', ProductsRouter)
+app.use('/users', UsersRouter)
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+app.listen(port, () => {
+    console.log('App server started at http://localhost:3000')
+})
---
+++ routers/index.js
@@ -0,0 +1,2 @@
+export { default as ProductsRouter } from './ProductsRouter.js'
+export { default as UsersRouter } from './UsersRouter.js'
