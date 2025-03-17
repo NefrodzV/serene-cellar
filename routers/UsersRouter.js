dc7aab9c7a180f6ea495a9c23fce67fae3f19c7c
Neftaly <neftalyrv23@gmail.com>
2025-03-17 18:31:06 -0400

    set up end points to get the user, get the cart, add items to the cart and delete items from the cart.

--- routers/UsersRouter.js
+++ routers/UsersRouter.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
     res.send('users index')
 })
 
+router.get('/:userId', (req, res) => {
+    res.send('GET call to get the user with id')
+})
+
+router.get('/:userId/cart', (req, res) => {
+    res.send('GET call to the cart items')
+})
+
+router.post('/:userId/cart', (req, res) => {
+    res.send('POST call to add a item to cart')
+})
+
+router.delete('/:userId/cart/:itemId', (req, res) => {
+    res.send('DELETE call to delete a item in cart')
+})
+
 export default router
