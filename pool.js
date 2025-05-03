076aa9b991cb1e6b6d08418b697bb73e6b1bd645
Neftaly <neftalyrv23@gmail.com>
2025-05-03 16:57:01 -0400

    updated to the connection string

--- pool.js
+++ pool.js
@@ -1,11 +1,7 @@
-import pg from "pg";
-import { configDotenv } from "dotenv";
-configDotenv();
-const { Pool } = pg;
+import pg from 'pg'
+import { configDotenv } from 'dotenv'
+configDotenv()
+const { Pool } = pg
 export default new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.ROLE_NAME,
-  database: process.env.DATABASE,
-  password: process.env.ROLE_PASSWORD,
-  port: 5432,
-});
+    connectionString: process.env.DB_URL,
+})
