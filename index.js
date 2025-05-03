dc20bfa5345a70437486c564a392d2213f6e105d
Neftaly <neftalyrv23@gmail.com>
2025-05-03 19:25:15 -0400

    implemented cors and installed jsonwebtokens and express validador

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import   express  from "express"
-import { ProductsRouter, UsersRouter } from "./routers/index.js"
+import express from 'express'
+import cors from 'cors'
+import { ProductsRouter, UsersRouter } from './routers/index.js'
 const app = express()
 const port = 3000
-
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use('/products', ProductsRouter)
 app.use('/users', UsersRouter)
 app.get('/', (req, res) => {
-    res.send('Hello Serene Cellar')
+    res.send('Hello! Serene Cellar!')
 })
-
 
 app.listen(port, () => {
     console.log('App server started at http://localhost:3000')
