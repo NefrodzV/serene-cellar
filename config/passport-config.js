aea15ff78c8d9359f0816df3a618a2f8225989c6
Neftaly <neftalyrv23@gmail.com>
2025-05-07 19:38:14 -0400

    configure passport and setup cookie parser

---
+++ config/passport-config.js
@@ -0,0 +1,37 @@
+import passport from 'passport'
+import passportJwt from 'passport-jwt'
+import pool from '../pool'
+import { configDotenv } from 'dotenv'
+configDotenv()
+
+const JwtStrategy = passportJwt.Strategy
+const ExtractJwt = passportJwt.ExtractJwt
+
+passport.use(
+    new JwtStrategy(
+        {
+            jwtFromRequest: (req) => {
+                let token = null
+                if (req && req.cookies) {
+                    token = req.cookies['sereneJwt']
+                }
+                return token
+            },
+            secretOrKey: process.env.JWT_SECRET,
+        },
+        async (jwt_payload, done) => {
+            try {
+                const { userId } = jwt_payload
+                const { row } = pool.query('SELECT * FROM users WHERE id=$1', [
+                    userId,
+                ])
+                if (row.length === 0) return done(null, false)
+                const user = row[0]
+                return done(null, user)
+            } catch (error) {
+                console.error('Database query error', error)
+                return done(error, false)
+            }
+        }
+    )
+)
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import { ProductsRouter, UsersRouter, AuthRouter } from './routers/index.js'
+import cookieParser from 'cookie-parser'
 const app = express()
 const port = 3000
 app.use(cors())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/products', ProductsRouter)
