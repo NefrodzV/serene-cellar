6f881535559f46505810d4e15ad502a97d3a20ce
Neftaly <neftalyrv23@gmail.com>
2025-05-18 12:51:23 -0400

    setup pool config to defined by a env variable

--- pool.js
+++ pool.js
@@ -2,7 +2,17 @@ import pg from 'pg'
 import { configDotenv } from 'dotenv'
 configDotenv()
 const { Pool } = pg
-export default new Pool({
-    connectionString: process.env.DB_URL,
-    secure: true,
-})
+const poolConfig =
+    process.env.NODE_ENV === 'development'
+        ? {
+              host: process.env.DB_HOST,
+              user: process.env.ROLE_NAME,
+              database: process.env.DATABASE,
+              password: process.env.ROLE_PASSWORD,
+              port: 5432,
+              secure: true,
+          }
+        : {
+              connectionString: process.env.DB_URL,
+          }
+export default new Pool(poolConfig)
