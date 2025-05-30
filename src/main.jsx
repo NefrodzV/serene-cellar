a2a1815181080e46a78baf3296ab527dc1c741c5
Neftaly <neftalyrv23@gmail.com>
2025-05-30 17:32:38 -0400

    set up google auth

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,11 @@ import { setupCounter } from './counter.js'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 createRoot(document.getElementById('root')).render(<React.StrictMode>
+<GoogleOAuthProvider clientId={googleClientId}>
   <Login />
+</GoogleOAuthProvider>
 </React.StrictMode>)
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,35 @@
+import { GoogleLogin } from "@react-oauth/google"
 import React from "react"
-export default function Login() {
+
+const sereneCellarApiUrl = import.meta.env.VITE_SERENE_CELLAR_API_URL
+
+export default function LoginPage() {
     return <div>
         <h1>Login</h1>
+        <GoogleLogin onSuccess={({credential}) => {
+            console.log(credential)
+            fetch(sereneCellarApiUrl + "/auth/google", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }, 
+                body: JSON.stringify({idToken: credential})
+            })
+            .then(response => {
+                if(!response.ok) {
+                    console.error('Network response not ok')
+                    return response.json()
+                }
+                return response.json()
+            })
+            .then(data => {
+                console.log('User google authenticated', data)
+            }, )
+            .catch(error => {
+                console.error(error)
+            })
+        }} onError={error => {
+            console.error('Google login failed: ', error)
+        }}/>
     </div>
-    
 }
