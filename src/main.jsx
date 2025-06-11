1022cc84ff9cdeb5902fc99b066a33cdad139c97
Neftaly <neftalyrv23@gmail.com>
2025-06-11 15:59:35 -0400

    setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './pages/Login.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { RouterProvider } from 'react-router-dom'
+import router from '../Router'
+
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 createRoot(document.getElementById('root')).render(<React.StrictMode>
 <GoogleOAuthProvider clientId={googleClientId}>
-  <Login />
+  <RouterProvider router={router} />
 </GoogleOAuthProvider>
 </React.StrictMode>)
