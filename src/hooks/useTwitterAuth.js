715b3b78a9c68b54709b60ccbec8f2f703c266ee
Neftaly <neftalyrv23@gmail.com>
2025-06-12 19:01:56 -0400

    removed unused code  changed the callback uri to the local domain of grok

--- src/hooks/useTwitterAuth.js
+++ src/hooks/useTwitterAuth.js
@@ -1,21 +1,19 @@
-import pkceChallenge ,{ generateChallenge } from "pkce-challenge"
-const sereneCellarApiUrl = import.meta.env.VITE_SERENE_CELLAR_API_URL
+import pkceChallenge from "pkce-challenge"
 const twitterClientId = import.meta.env.VITE_TWITTER_CLIENT_ID
 
 export default function useTwitterAuth() {
     const authenticate = async () => {
-
         const { code_verifier, code_challenge } =  await pkceChallenge()
         localStorage.setItem('code_verifier', code_verifier)
 
         const params = new URLSearchParams({
             response_type: 'code',
             client_id: twitterClientId,
-            redirect_uri: 'https://a1f5-2606-6a40-7-6586-b7a9-839c-3e9b-31b4.ngrok-free.app/twitter/callback',
-            scope: 'tweet.read users.read',
+            redirect_uri: 'https://nefrodzv23.ngrok.io/twitter/callback',
+            scope: 'tweet.read users.read offline.access',
             state: 'Some random state',
             code_challenge: code_challenge,
-            code_challenge_method: 'plain'
+            code_challenge_method: 'S256'
         })
 
         window.location.href  = `https://twitter.com/i/oauth2/authorize?${params.toString()}`
