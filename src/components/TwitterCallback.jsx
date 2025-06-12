016ac13a4f7b60fb099a6400d6b1954b1224f8ab
Neftaly <neftalyrv23@gmail.com>
2025-06-12 19:01:02 -0400

    Implemented the twitter callback logic

--- src/components/TwitterCallback.jsx
+++ src/components/TwitterCallback.jsx
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 export default function TwitterCallback() {
+    const[searchParams] = useSearchParams()
+    const code = searchParams.get('code')
+    const state = searchParams.get('state')
+    useEffect(() => {
+        const authenticate = async () => {
+            try {
+                const code_verifier = localStorage.getItem('code_verifier')
+                console.log(code_verifier)
+                const response = await fetch('http://localhost:3000/auth/twitter', {
+                    method: 'post',
+                    headers: {
+                        'Content-Type':'application/json'
+                    },
+                    body: JSON.stringify({
+                        code,
+                        state,
+                        code_verifier
+                    })
+                })
+                if(!response.ok) {
+                    console.error(await response.json())
+                    return
+                }
+            } catch(error) {
+                console.error('Twitter callback authentication -> ', error)
+            }
+        } 
+        authenticate()
+    },[])
     return <p>Login you in to your account please wait...</p>
 }
