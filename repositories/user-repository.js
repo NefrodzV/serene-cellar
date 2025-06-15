109d7ffa5e4056b4d70b2f2e76dc0b592aea74d3
Neftaly <neftalyrv23@gmail.com>
2025-06-14 20:02:07 -0400

    Change the db connection to its own directory. Also, made the repository layer for the user.

---
+++ repositories/user-repository.js
@@ -0,0 +1,57 @@
+import db from '../db/index.js'
+export const userRepository = {
+    async findByEmail(email) {
+        const { rows } = await db.query(`SELECT *  FROM users WHERE email=$1`, [
+            email,
+        ])
+        return rows[0]
+    },
+
+    async createUserWithEmail({ username, password, email }) {
+        const { rows } = await db.query(
+            `INSERT INTO users (username, email, password)
+            VALUES ($1,$2,$3) RETURNING id`,
+            [username, email, password]
+        )
+
+        return rows[0]
+    },
+
+    async createUserWithTwitter({ twitterId, username }) {
+        const { rows } = await db.query(
+            `INSERT INTO users (twitter_id, username)
+            VALUES ($1,$2) RETURNING id`,
+            [twitterId, username]
+        )
+
+        return rows[0]
+    },
+
+    async createUserWithGoogle({ googleId, email, username }) {
+        const { rows } = await db.query(
+            `INSERT INTO users (google_id, username, email)
+            VALUES ($1,$2,$3) RETURNING id`,
+            [googleId, username, email]
+        )
+
+        return rows[0]
+    },
+
+    async findByGoogleId(id) {
+        const { rows } = await db.query(
+            `SELECT * FROM users 
+            WHERE google_id=$1`,
+            [id]
+        )
+        return rows[0]
+    },
+
+    async findByTwitterId(id) {
+        const { rows } = await db.query(
+            `SELECT *  FROM users 
+            WHERE twitter_id=$1`,
+            [id]
+        )
+        return rows[0]
+    },
+}
