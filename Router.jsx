b61e4642587a49d21f0f48851d02419ee2922e7b
Neftaly <neftalyrv23@gmail.com>
2025-06-26 19:14:39 -0400

    Added the shop page

--- Router.jsx
+++ Router.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import { LoginPage } from "./src/pages";
+import { LoginPage, ShopPage } from "./src/pages";
 import {TwitterCallback, TwitterAuth} from "./src/components";
 const router = createBrowserRouter([
     {
         path: '/',
-        element : <Navigate to={'/login'} />
+        element : <Navigate to={'/shop'} />
+    },
+    {
+        path: '/shop',
+        element: <ShopPage />
     },
     {
         path: '/login',
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,19 @@
-import react from "react";
+import React from "react"
+import { ProductList } from "../components"
 export default function ShopPage() {
-    return <div>Hello Shop page</div>
+    return <>
+    <header>
+        <div>Logo</div>
+        <nav>
+            {/* TODO: USE ELEMENTS HERE FOR NAVIGATION FROM REACT ROUTER */}
+            <a href="http://">Home</a>
+            <a href="http://">Login</a>
+            <a href="http://">Cart</a>
+        </nav>
+        <main>Product Catalog here
+            <br />
+            <ProductList />
+        </main>
+    </header>
+    </>
 }
