90f121ad6d4e3f5be138407d542b311ca7ee7d79
Neftaly <neftalyrv23@gmail.com>
2025-06-29 13:54:51 -0400

    Added elements to the product card

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function ProductCard({product}) {
-    console.log(product)
-    return <a href={`/shop/${product.id}`}>{product.name}</a>
+    const { images, id, name, price } = product
+    return (<Link to={`/shop/${name.toLowerCase().split(' ').join('-')}`} state={{product} }>
+    <img 
+    width={150}
+    srcSet={`${images.phone} 360w, ${images.tablet} 720w, ${images.desktop} 1080w`}
+    sizes={`(max-width: 600px) 360px, (max-width: 1024px) 720px, 1080px`}
+    /> 
+    <h3>{name}</h3>
+    <p>{`From $${price.sixPack.value}`}</p>
+    </Link>)
 }
