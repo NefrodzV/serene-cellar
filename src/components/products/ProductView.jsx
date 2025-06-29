769c367b32632e6eee011a768f9d62013fb3bc4f
Neftaly <neftalyrv23@gmail.com>
2025-06-28 22:09:49 -0400

    Added select elements for quantity and packSize. Also setup their handlers and made the calculation for the total.

--- src/components/products/ProductView.jsx
+++ src/components/products/ProductView.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useProduct } from "../../hooks";
 
 export function ProductView() {
     const [product, isLoading] = useProduct()
-    console.log(product)
+    const { name, price, category, abv, ml, description,  images } = product
+    const [packSize, setPackSize] = useState(Object.keys(price).at(0))
+    const [quantity, setQuantity] = useState(1)
+    function packSizeHandler(e) {
+        setPackSize(e.target.value)
+    }
+    function quantityHandler(e) {
+        setQuantity(e.target.value)
+    }
+
     return (
-        <h1>Product View</h1>
-    //     <article>
-    //     <img 
-    // width={500} // This should be controlled by css
-    // srcSet={`${images.phone} 360w, ${images.tablet} 720w, ${images.desktop} 1080w`}
-    // sizes={`(max-width: 600px) 360px, (max-width: 1024px) 720px, 1080px`}
-    // /> 
-    // <h1></h1></article>
+    <>
+        <h1>{name}</h1>
+        <p>{description}</p>
+        <label htmlFor="packSize">Pack Size</label>
+        <select id="packSize" onChange={packSizeHandler}>
+            {Object.entries(price).map(([key, {unit , value}]) => (
+                <option value={key} key={key}>{unit} - ${value}</option>
+            ))}
+        </select>
+        <label htmlFor="quantity">Quantity</label>
+        <select id="quantity" onChange={quantityHandler}>
+            {[...Array(10)].map((_, i) => (
+                <option key={i + 1}>{i + 1}</option>
+            ))}
+        </select>
+        <p>{`Total: $${Number(price[packSize]?.value) * Number(quantity)}` }</p>
+        
+    <img    
+    width={'25%'}
+    srcSet={`${images.phone} 360w, ${images.tablet} 720w, ${images.desktop} 1080w`}
+    sizes={`(max-width: 600px) 360px, (max-width: 1024px) 720px, 1080px`}
+    /> 
+    </>
+        
+   
     )
 }
