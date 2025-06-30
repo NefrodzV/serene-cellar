e5581fee14c50ae124ea3db1f8a7139c82a40b46
Neftaly <neftalyrv23@gmail.com>
2025-06-30 18:38:11 -0400

    Refactor with prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { RouterProvider } from 'react-router-dom'
 import router from '../Router'
-
+import { UserProvider } from './contexts/UserContext'
+import { CartProvider } from './contexts/CartContext'
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
-createRoot(document.getElementById('root')).render(<React.StrictMode>
-<GoogleOAuthProvider clientId={googleClientId}>
-  <RouterProvider router={router} />
-</GoogleOAuthProvider>
-</React.StrictMode>)
+createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <UserProvider>
+            <CartProvider>
+                <GoogleOAuthProvider clientId={googleClientId}>
+                    <RouterProvider router={router} />
+                </GoogleOAuthProvider>
+            </CartProvider>
+        </UserProvider>
+    </React.StrictMode>
+)
