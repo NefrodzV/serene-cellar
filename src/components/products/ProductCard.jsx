eb556b2575e4d2de4f8dd296ddfe1ce40c8a7208
Neftaly <neftalyrv23@gmail.com>
2025-07-05 15:42:53 -0400

    Refactor

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-export default function ProductCard({product}) {
+import React from 'react'
+import { Link } from 'react-router-dom'
+export function ProductCard({ product }) {
     const { images, id, name, price } = product
-    return (<Link to={`/shop/${name.toLowerCase().split(' ').join('-')}`} state={{product} }>
-    <img 
-    width={150}
-    srcSet={`${images.phone} 360w, ${images.tablet} 720w, ${images.desktop} 1080w`}
-    sizes={`(max-width: 600px) 360px, (max-width: 1024px) 720px, 1080px`}
-    /> 
-    <h3>{name}</h3>
-    <p>{`From $${price.sixPack.value}`}</p>
-    </Link>)
+    return (
+        <Link
+            to={`/shop/${name.toLowerCase().split(' ').join('-')}`}
+            state={{ product }}
+        >
+            <img
+                width={150}
+                srcSet={`${images.phone} 360w, ${images.tablet} 720w, ${images.desktop} 1080w`}
+                sizes={`(max-width: 600px) 360px, (max-width: 1024px) 720px, 1080px`}
+            />
+            <h3>{name}</h3>
+            <p>{`From $${price.sixPack.value}`}</p>
+        </Link>
+    )
 }
