23e501bc6d9aa19ab77810480fc7d07cebbd0c0b
Neftaly <neftalyrv23@gmail.com>
2025-07-14 17:29:08 -0400

    Removed toher elements because its been passed to main layout

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,22 +3,9 @@ import { ProductList } from '../components'
 import { Link } from 'react-router-dom'
 
 export function ShopPage() {
-    return (
-        <>
-            <header>
-                <div>Logo</div>
-                <nav>
-                    {/* TODO: USE ELEMENTS HERE FOR NAVIGATION FROM REACT ROUTER */}
-                    <a href="http://">Home</a>
-                    <a href="http://">Login</a>
-                    <Link to={'/cart'}>Cart</Link>
-                </nav>
-                <main>
-                    Product Catalog here
-                    <br />
-                    <ProductList />
-                </main>
-            </header>
-        </>
-    )
+  return (
+    <>
+      <ProductList />
+    </>
+  )
 }
