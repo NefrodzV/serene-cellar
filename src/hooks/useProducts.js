3bb9f13533b5328d0698847bb9c3518b7916b976
Neftaly <neftalyrv23@gmail.com>
2025-07-19 06:17:42 -0400

    Changed the function to be in a service and added a initializer

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,27 +1,21 @@
-import { useEffect, useState } from "react";
-import { useLoading } from "./useLoading";
+import { useEffect, useState } from 'react'
+import { useLoading } from './useLoading'
+import { getProducts } from '../services/productService'
 
 export function useProducts() {
-    const [products, setProducts] = useState([])
-    const isLoading = useLoading(products)
+  const [products, setProducts] = useState([])
+  const isLoading = useLoading(products)
 
-    useEffect(() => {
-        async function getProducts() {
-            const apiUrl = import.meta.env.VITE_SERENE_CELLAR_API_URL
-            try {
-                const response = await fetch(apiUrl + '/products')
-                const data = await response.json()
-                if(!response.ok) {
-                    return console.error('Get products response error:', data)
-                }
-                setProducts(data)
-            } catch (e) {
-                console.error('Some error happened fetching products:', e)
-            } 
-        }
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const products = await getProducts()
+        setProducts(products)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [])
 
-        getProducts()
-    },[])
-
-    return [products, isLoading]
+  return [products, isLoading]
 }
