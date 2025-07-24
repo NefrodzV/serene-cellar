fe8dc70ace693fd931a1cf552bb2d66c9a26809d
Neftaly <neftalyrv23@gmail.com>
2025-07-24 15:54:02 -0400

    remove message

--- middlewares/validationHandler.js
+++ middlewares/validationHandler.js
@@ -3,7 +3,6 @@ export const validate = (req, res, next) => {
     const result = validationResult(req)
     if (!result.isEmpty()) {
         return res.status(400).json({
-            message: 'Errors in request',
             errors: result.array(),
         })
     }
