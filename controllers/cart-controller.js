e011e0b2f2406024cbbf0740ce5cbc8ba912ec48
Neftaly <neftalyrv23@gmail.com>
2025-07-27 11:55:58 -0400

    refactor the logic to get cart item and addItem to its repository

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -2,122 +2,74 @@ import { body, param, validationResult, matchedData } from 'express-validator'
 import { passport } from '../config/index.js'
 import pool from '../db/pool.js'
 import { validate } from '../middlewares/validationHandler.js'
+import {
+    createCartItem,
+    getCardByUserId,
+    getCartItemByCartProductAndUnit,
+    getCartItemsWithProductData,
+    updateCartItemQuantity,
+} from '../repositories/cart-repository.js'
 
 /**
  * Make the requests return the update cartItems
  */
 const getCart = [
     passport.authenticate('jwt', { session: false }),
-    param('cartId')
-        .exists({ values: 'falsy' })
-        .withMessage('Item id must be defined')
-        .bail()
-        .isInt()
-        .withMessage('Item ID must be a valid integer'),
     validate,
     async (req, res, next) => {
-        const cartId = req.params.cartId
         try {
-            const { rows } = await pool.query(
-                `
-                SELECT 
-                ci.id, 
-                ci.quantity, 
-                ci.unit_price, 
-                ci.unit_type,
-                ci.product_id,
-                p.slug,
-                (
-                    SELECT json_object_agg(pi.device_type, pi.image_url)
-                    FROM product_images pi
-                    WHERE pi.product_id = ci.product_id
-                ) as images
-                FROM cart_items ci
-                INNER JOIN products p ON p.id = ci.product_id
-                WHERE ci.cart_id = $1
-                `,
-                [cartId]
-            )
-            return res.json({
-                cart: rows,
-            })
+            const cart = await getCardByUserId(req.user.id)
+            if (!cart)
+                return res.status(404).json({ message: 'Cart not found' })
+            const items = await getCartItemsWithProductData(cart.id)
+            return res.json({ cart: { ...cart, items } })
         } catch (error) {
             next(error)
         }
     },
 ]
-
+/**TODO: Update the other function to remove the id param
+ * and add functions to the repository*/
 const addItem = [
     passport.authenticate('jwt', { session: false }),
-    param('cartId')
-        .exists({ values: 'falsy' })
-        .withMessage('Cart id must be defined')
-        .bail()
-        .isInt()
-        .withMessage('Cart id must be a valid integer'),
     body('item')
         .exists({ values: 'falsy' })
         .withMessage('No item defined to add to cart'),
     validate,
 
     async (req, res, next) => {
-        const cartId = parseInt(req.params.cartId)
         const data = matchedData(req)
         try {
-            const existsResult = await pool.query(
-                `
-                SELECT EXISTS (
-                    SELECT * FROM cart_items
-                    WHERE product_id=$1 AND unit_type=$2 AND cart_id=$3
-                )`,
-                [data.productId, data.unitType, cartId]
+            const cart = await getCardByUserId(req.user.id)
+            const existingItem = await getCartItemByCartProductAndUnit(
+                cart.id,
+                data.productId,
+                data.unitType
             )
-            if (existsResult.rows[0].exists) {
-                await pool.query(
-                    `
-                    UPDATE cart_items
-                    SET quantity = quantity + $1
-                    WHERE product_id=$2 AND unit_type=$3 AND cart_id=$4
-                    `,
-                    [data.quantity, data.productId, data.unitType, cartId]
-                )
+            let status = null
+            let message = null
+
+            if (existingItem) {
+                await updateCartItemQuantity(existingItem.id, data.quantity)
+                status = 200
+                message = 'Cart item updated'
             } else {
-                await pool.query(
-                    `INSERT INTO cart_items 
-                    (product_id, cart_id, quantity, unit_price, unit_type)
-                    VALUES ($1, $2, $3, $4, $5)`,
-                    [
-                        data.productId,
-                        cartId,
-                        data.quantity,
-                        data.unitPrice,
-                        data.unitType,
-                    ]
+                await createCartItem(
+                    cart.id,
+                    data.productId,
+                    data.quantity,
+                    data.unitPrice,
+                    data.unitType
                 )
+                status = 201
+                message = 'Cart item added'
             }
 
-            // Getting updated cart
-            const { rows } = await pool.query(
-                `
-                SELECT 
-                ci.id, 
-                ci.quantity, 
-                ci.unit_price, 
-                ci.unit_type,
-                ci.product_id,
-                p.slug,
-                (
-                    SELECT json_object_agg(pi.device_type, pi.image_url)
-                    FROM product_images pi
-                    WHERE pi.product_id = ci.product_id
-                ) as images
-                FROM cart_items ci
-                INNER JOIN products p ON p.id = ci.product_id
-                WHERE ci.cart_id = $1
-                `,
-                [cartId]
-            )
-            return res.status(201).json({ cart: rows })
+            // Getting updated cart items
+            const cartItems = await getCartItemsWithProductData(cart.id)
+            return res
+                .status(status)
+                .json({ message, cart: { ...cart, items: cartItems } })
         } catch (error) {
             next(error)
         }
---
+++ repositories/cart-repository.js
@@ -0,0 +1,77 @@
+import db from '../db/index.js'
+
+export async function getCardByUserId(userId) {
+    const { rows } = await db.query(
+        `
+        SELECT id 
+        FROM cart 
+        WHERE user_id=$1`,
+        [userId]
+    )
+    return rows[0] || null
+}
+
+export async function getCartItemsWithProductData(cartId) {
+    const { rows } = await db.query(
+        `SELECT 
+        ci.id, 
+        ci.quantity, 
+        ci.unit_price AS price, 
+        ci.unit_type,
+        p.name,
+        p.slug,
+        (   
+            SELECT
+            jsonb_object_agg(pi.device_type, pi.image_url)
+            FROM product_images pi
+            WHERE pi.product_id = p.id
+        ) as images
+        FROM cart_items ci
+        INNER JOIN products p ON ci.product_id = p.id
+        WHERE cart_id=$1`,
+        [cartId]
+    )
+
+    return rows
+}
+
+export async function updateCartItemQuantity(itemId, quantity) {
+    await pool.query(
+        `
+        UPDATE cart_items
+        SET quantity = GREATEST(quantity + $1, 0)
+        WHERE id=$2
+        `,
+        [quantity, itemId]
+    )
+}
+
+export async function getCartItemByCartProductAndUnit(
+    cartId,
+    productId,
+    unitType
+) {
+    const { rows } = await pool.query(
+        `
+        SELECT id FROM cart_items
+        WHERE product_id=$1 AND unit_type=$2 AND cart_id=$3
+        )`,
+        [productId, unitType, cartId]
+    )
+    return rows[0] || null
+}
+
+export async function createCartItem(
+    cartId,
+    productId,
+    quantity,
+    unitPrice,
+    unitType
+) {
+    await pool.query(
+        `INSERT INTO cart_items 
+        (product_id, cart_id, quantity, unit_price, unit_type)
+        VALUES ($1, $2, $3, $4, $5)`,
+        [productId, cartId, quantity, unitPrice, unitType]
+    )
+}
