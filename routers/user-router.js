6309f28971cbbebf4448df488ec44ebbf29128a0
Neftaly <neftalyrv23@gmail.com>
2025-07-26 20:10:38 -0400

    Added  a /me route

--- routers/user-router.js
+++ routers/user-router.js
@@ -1,23 +1,12 @@
 import express from 'express'
-import { userController } from '../controllers/index.js'
+import { cartController, userController } from '../controllers/index.js'
 const router = express.Router()
 
-router.get('/:userId/cart', (req, res) => {
-    res.send('GET call to the cart items')
-})
+router.get('/', userController.getUser)
 
-router.post('/:userId/cart', (req, res) => {
-    res.send('POST call to add a item to cart')
-})
-
-router.delete('/:userId/cart/:itemId', (req, res) => {
-    res.send('DELETE call to delete a item in cart')
-})
-
-router.get('/me', userController.getUser)
-
-router.get('/:userId', (req, res) => {
-    res.send('GET call to get the user with id')
-})
+router.get('/cart', cartController.getCart)
+router.post('/cart', cartController.addItem)
+router.put('/cart/:itemId', cartController.updateItem)
+router.delete('/cart/:itemId', cartController.deleteItem)
 
 export default router
