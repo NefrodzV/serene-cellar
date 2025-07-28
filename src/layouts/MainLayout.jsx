38c5a2e3f7fc33e5b764acdbe590d82bec4dd540
Neftaly <neftalyrv23@gmail.com>
2025-07-28 14:39:12 -0400

    Added logic to show login or profie if user is logged in

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import { Outlet, Link, NavLink } from 'react-router-dom'
-import { useCart } from '../hooks'
+import { useCart, useUser } from '../hooks'
 import { MessageContainer } from '../components/messages/MessageContainer'
 export function MainLayout() {
   const { totalItems } = useCart()
+  const { isAuthenticated, user } = useUser()
   return (
     <div className="app-layout">
       <header>
         <div>Logo</div>
         <nav>
           {/* TODO: USE ELEMENTS HERE FOR NAVIGATION FROM REACT ROUTER */}
-          <a href="http://">Home</a>
+          <NavLink href="#">Home</NavLink>
           <NavLink to={'/shop'}>Shop</NavLink>
-          <NavLink to={'/login'}>Login</NavLink>
-          <Link to={'/cart'}>Cart {totalItems}</Link>
+          {isAuthenticated ? (
+            <NavLink to={'#'}>{user.username}</NavLink>
+          ) : (
+            <NavLink to={'/login'}>Login</NavLink>
+          )}
+          <NavLink to={'/cart'}>Cart {totalItems}</NavLink>
         </nav>
       </header>
       <main className="main-layout">
         <Outlet />
         <MessageContainer />
       </main>
-
       <footer>Footer</footer>
     </div>
   )
