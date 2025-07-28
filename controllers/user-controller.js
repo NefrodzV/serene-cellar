6b08b9a750331c18957d8db1bc098777459f4655
Neftaly <neftalyrv23@gmail.com>
2025-07-28 14:37:32 -0400

    Set a comment

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 import { passport } from '../config/index.js'
 const getUser = [
     passport.authenticate('jwt', { session: false }),
+    // TODO: Implement the data that will be returned to the user
     (req, res) => {
         res.json({ message: 'Authentication sucessful', user: req.user })
     },
