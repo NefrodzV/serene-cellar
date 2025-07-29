9c1539d688d9dd14341e9394210c1db3cba9f98a
Neftaly <neftalyrv23@gmail.com>
2025-07-29 18:07:23 -0400

    Changed typo and made the local cart items delete when sync is successful

--- backend/repositories/cart-repository.js
+++ backend/repositories/cart-repository.js
@@ -1,19 +1,20 @@
 import db from '../db/index.js'
 
-export async function getCardByUserId(userId) {
-    const { rows } = await db.query(
-        `
+export async function getCartByUserId(userId) {
+  const { rows } = await db.query(
+    `
         SELECT id 
         FROM cart 
         WHERE user_id=$1`,
-        [userId]
-    )
-    return rows[0] || null
+    [userId]
+  )
+
+  return rows[0] || null
 }
 
 export async function getCartItemsWithProductData(cartId) {
-    const { rows } = await db.query(
-        `SELECT 
+  const { rows } = await db.query(
+    `SELECT 
         ci.id, 
         ci.quantity, 
         ci.unit_price AS price, 
@@ -29,71 +30,71 @@ export async function getCartItemsWithProductData(cartId) {
         FROM cart_items ci
         INNER JOIN products p ON ci.product_id = p.id
         WHERE cart_id=$1`,
-        [cartId]
-    )
+    [cartId]
+  )
 
-    return rows
+  return rows
 }
 
 export async function updateCartItemQuantity(itemId, quantity) {
-    await db.query(
-        `
+  await db.query(
+    `
         UPDATE cart_items
         SET quantity = GREATEST(quantity + $1, 0)
         WHERE id=$2
         `,
-        [quantity, itemId]
-    )
+    [quantity, itemId]
+  )
 }
 
 export async function getCartItemByCartProductAndUnit(
-    cartId,
-    productId,
-    unitType
+  cartId,
+  productId,
+  unitType
 ) {
-    const { rows } = await db.query(
-        `
+  const { rows } = await db.query(
+    `
         SELECT id FROM cart_items
         WHERE product_id=$1 AND unit_type=$2 AND cart_id=$3
         )`,
-        [productId, unitType, cartId]
-    )
-    return rows[0] || null
+    [productId, unitType, cartId]
+  )
+  return rows[0] || null
 }
 
 export async function createCartItem(
-    cartId,
-    productId,
-    quantity,
-    unitPrice,
-    unitType
+  cartId,
+  productId,
+  quantity,
+  unitPrice,
+  unitType
 ) {
-    await db.query(
-        `INSERT INTO cart_items 
+  await db.query(
+    `INSERT INTO cart_items 
         (product_id, cart_id, quantity, unit_price, unit_type)
         VALUES ($1, $2, $3, $4, $5)`,
-        [productId, cartId, quantity, unitPrice, unitType]
-    )
+    [productId, cartId, quantity, unitPrice, unitType]
+  )
 }
 
 export async function deleteCartItem(itemId) {
-    await db.query(
-        `DELETE FROM cart_items 
+  await db.query(
+    `DELETE FROM cart_items 
         WHERE id=$1`,
-        [itemId]
-    )
+    [itemId]
+  )
 }
 
 export async function getItemsByCartId(cartId) {
-    const { rows } = await db.query(
-        `
+  const { rows } = await db.query(
+    `
         SELECT 
         product_id,
         unit_type 
         FROM cart_items 
         WHERE cart_id=$1`,
-        [cartId]
-    )
+    [cartId]
+  )
 
-    return rows
+  return rows
 }
