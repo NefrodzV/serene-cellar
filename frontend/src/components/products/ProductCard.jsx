b5ad858dc0545f37bfdbfcbc171df459419b421d
Neftaly <neftalyrv23@gmail.com>
2025-08-31 09:51:49 -0400

    Changed the prices object to match the backend response

--- frontend/src/components/products/ProductCard.jsx
+++ frontend/src/components/products/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 export function ProductCard({ product }) {
-  const { images, id, name, price, slug } = product
+  const { images, id, name, prices, slug } = product
   return (
     <li>
       <Link className="card" to={`/shop/${slug}`} state={{ product }}>
@@ -11,7 +11,7 @@ export function ProductCard({ product }) {
           sizes={`(max-width: 600px) 360px, (max-width: 1024px) 720px, 1080px`}
         />
         <h3>{name}</h3>
-        <p>{`From $${price.sixPack.value}`}</p>
+        <p>{`From $${prices.SIX_PACK.value}`}</p>
       </Link>
     </li>
   )
