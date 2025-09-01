38905dab94df9d1d0eac15e2229e637405a9e92a
Neftaly <neftalyrv23@gmail.com>
2025-09-01 19:26:08 -0400

    Created a constant file for error messages

---
+++ frontend/src/constants/ErrorMessages.js
@@ -0,0 +1,6 @@
+export const ErrorsMessages = {
+  PRODUCT_UNAVAILABLE:
+    'This product is currently unavailable. We’re sorry for the inconvenience.',
+  OUT_OF_STOCK:
+    'Sorry, this product is currently out of stock. We appreciate your understanding.',
+}
