3493ad015220fb0fe5f8d770f4591beea37c35ef
Neftaly <neftalyrv23@gmail.com>
2025-09-03 19:07:53 -0400

    Made fields as final unit price, line total  (without discount), final line total price (with discount). Also fixed some issue with the stock calculation.

--- backend/repositories/cart-repository.js
+++ backend/repositories/cart-repository.js
@@ -6,7 +6,7 @@ export async function getCartByUserId(userId) {
     `
     SELECT 
     COALESCE(bool_and(item.purchasable), false) as can_checkout,
-    COALESCE(SUM(item.quantity * item.unit_price), 0) as subtotal,
+    COALESCE(SUM(line_total), 0) as subtotal,
     COALESCE(json_agg(item), '[]') as items,
     COALESCE(SUM(item.quantity), 0) as total_items,
     CASE 
@@ -15,29 +15,49 @@ export async function getCartByUserId(userId) {
     END as is_empty
     FROM
     (SELECT 
+        p.name,
         ci.id, 
         ci.quantity, 
         ci.unit_price, 
         ci.unit_type,
+        CASE WHEN p.discount_percent <> 0
+          THEN ROUND(ci.quantity * (ci.unit_price - (ci.unit_price * p.discount_percent/100)), 2)
+          ELSE ci.quantity * ci.unit_price 
+        END as final_line_total,
+        (ci. unit_price * ci.quantity) as line_total,
+        (
+          SELECT pr.stock_quantity FROM prices pr 
+          WHERE ci.product_id = pr.product_id AND pr.unit = ci.unit_type
+        ) as stock,
         CASE
           WHEN p.discount_percent IS NOT NULL 
-          THEN ROUND(ci.unit_price * (1 - p.discount_percent / 100), 2)
+          THEN ROUND(ci.unit_price - (ci.unit_price * p.discount_percent / 100), 2)
           ELSE unit_price
-        END as effective_price,
+        END as final_unit_price,
         ARRAY_REMOVE(ARRAY[
-          CASE WHEN p.stock < ci.quantity THEN 'INSUFFICIENT_STOCK' END,
-          CASE WHEN p.stock <= 0 THEN 'OUT_OF_STOCK' END,
+          CASE WHEN 
+          (
+            SELECT pr.stock_quantity FROM prices pr 
+            WHERE ci.product_id = pr.product_id AND ci.unit_type = pr.unit
+          ) < ci.quantity THEN 'INSUFFICIENT_STOCK' END,
+          CASE WHEN (
+            SELECT pr.stock_quantity FROM prices pr 
+            WHERE ci.product_id = pr.product_id AND ci.unit_type = pr.unit
+          ) = 0 THEN 'OUT_OF_STOCK' END,
           CASE WHEN p.status <> 'active' THEN 'PRODUCT_UNAVAILABLE' END], null)
         AS errors,
         CASE
           WHEN p.status <> 'active' THEN false
-          WHEN p.stock <= 0 THEN false
-          WHEN ci.quantity > p.stock THEN false
+          WHEN (
+            SELECT pr.stock_quantity FROM prices pr 
+            WHERE ci.product_id = pr.product_id AND ci.unit_type = pr.unit
+          ) = 0 THEN false
+          WHEN ci.quantity > COALESCE((
+            SELECT pr.stock_quantity FROM prices pr 
+            WHERE ci.product_id = pr.product_id AND ci.unit_type = pr.unit
+          ),0) THEN false
           ELSE true
         END AS purchasable,
-        p.name,
-        p.slug,
-        p.stock,
         (   
             SELECT
             jsonb_object_agg(pi.device_type, pi.image_url)
