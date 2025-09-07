243520370a31cef1f4d69c2d04817a1a925fc487
Neftaly <neftalyrv23@gmail.com>
2025-09-07 13:54:24 -0400

    deleted some logic for updating quantity which will be done in the cart.

--- frontend/src/hooks/useProduct.js
+++ frontend/src/hooks/useProduct.js
@@ -1,30 +1,27 @@
 import { useState, useEffect } from 'react'
 import { useLoading } from './useLoading'
 import { API_URL } from '../config'
-export function useProduct(slug, isEditing) {
-    const [product, setProduct] = useState(null)
-    const isLoading = useLoading(product)
+export function useProduct(slug) {
+  const [product, setProduct] = useState(null)
+  const isLoading = useLoading(product)
 
-    useEffect(() => {
-        async function getProductWithSlug() {
-            try {
-                const response = await fetch(
-                    `${API_URL}/products/${slug.trim()}`
-                )
-                const data = await response.json()
+  useEffect(() => {
+    async function getProductWithSlug() {
+      try {
+        const response = await fetch(`${API_URL}/products/${slug.trim()}`)
+        const data = await response.json()
 
-                if (!response.ok) {
-                    return console.error('Error response:', data)
-                }
-                setProduct(data)
-            } catch (e) {
-                console.error('GET product with slug error:', e)
-            }
+        if (!response.ok) {
+          return console.error('Error response:', data)
         }
+        setProduct(data)
+      } catch (e) {
+        console.error('GET product with slug error:', e)
+      }
+    }
 
-        if (!product) getProductWithSlug()
-        if (isEditing) getProductWithSlug()
-    }, [slug, isEditing])
+    if (!product) getProductWithSlug()
+  }, [slug])
 
-    return [product, isLoading]
+  return [product, isLoading]
 }
