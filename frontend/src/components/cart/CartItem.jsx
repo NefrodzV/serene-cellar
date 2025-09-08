f90a2d10aa37fb1bf86799db36ffa76351045775
Neftaly <neftalyrv23@gmail.com>
2025-09-08 19:03:05 -0400

    Made the cart items responsive and made them with grid

--- frontend/src/components/cart/CartItem.jsx
+++ frontend/src/components/cart/CartItem.jsx
@@ -55,71 +55,72 @@ export function CartItem({ item }) {
           </p>
 
           <p>Available: {stock}</p>
-          <div className="item-control">
-            <button
-              disabled={quantity === MIN_ITEM_QUANTITY}
-              className="button primary"
-              onClick={() => decrement(item, Number(quantity))}
-            >
-              -
-            </button>
 
-            <input
-              onChange={(e) => {
-                setError('')
-                const value = e.target.value.replace(/\D/g, '')
-                if (Number(value) > stock) {
-                  setError('Quantity unavailable')
-                }
-                setRawQuantity(value)
-              }}
-              name="quantity"
-              style={{
-                width: `${rawQuantity.length}ch`,
-              }}
-              value={rawQuantity}
-              className="input primary quantity"
-              type="text"
-              inputMode="numeric"
-              pattern="[0-9]*"
-              min={MIN_ITEM_QUANTITY}
-              max={stock}
-              onBlur={(e) => {
-                setError('')
-                if (rawQuantity === '' || Number(rawQuantity) <= 0) {
-                  setRawQuantity(String(quantity))
-                  return
-                }
-                const val = rawQuantity.replace(/^0+(?!$)/g, '')
-                setRawQuantity(val)
-                if (val > stock) {
-                  setRawQuantity(String(quantity))
-                  return
-                }
-                updateItem(item, Number(rawQuantity))
-              }}
-            />
-
-            <button
-              disabled={quantity >= stock}
-              className="button primary"
-              onClick={() => increment(item, Number(rawQuantity))}
-            >
-              +
-            </button>
-
-            <button
-              aria-label="Delete cart item"
-              className="button primary delete"
-              type="button"
-              onClick={() => {
-                deleteItem(item)
-              }}
-            >
-              <i class="fa-solid fa-trash"></i>
-            </button>
-          </div>
           {error && <div className="error">{error}</div>}
+        </div>
+        <div className="item-control">
+          <button
+            disabled={quantity === MIN_ITEM_QUANTITY}
+            className="button primary"
+            onClick={() => decrement(item, Number(quantity))}
+          >
+            -
+          </button>
+
+          <input
+            onChange={(e) => {
+              setError('')
+              const value = e.target.value.replace(/\D/g, '')
+              if (Number(value) > stock) {
+                setError('Quantity unavailable')
+              }
+              setRawQuantity(value)
+            }}
+            name="quantity"
+            style={{
+              width: `${rawQuantity.length}ch`,
+            }}
+            value={rawQuantity}
+            className="input primary quantity"
+            type="text"
+            inputMode="numeric"
+            pattern="[0-9]*"
+            min={MIN_ITEM_QUANTITY}
+            max={stock}
+            onBlur={(e) => {
+              setError('')
+              if (rawQuantity === '' || Number(rawQuantity) <= 0) {
+                setRawQuantity(String(quantity))
+                return
+              }
+              const val = rawQuantity.replace(/^0+(?!$)/g, '')
+              setRawQuantity(val)
+              if (val > stock) {
+                setRawQuantity(String(quantity))
+                return
+              }
+              updateItem(item, Number(rawQuantity))
+            }}
+          />
+
+          <button
+            disabled={quantity >= stock}
+            className="button primary"
+            onClick={() => increment(item, Number(rawQuantity))}
+          >
+            +
+          </button>
+
+          <button
+            aria-label="Delete cart item"
+            className="button primary delete"
+            type="button"
+            onClick={() => {
+              deleteItem(item)
+            }}
+          >
+            <i class="fa-solid fa-trash"></i>
+          </button>
         </div>
       </article>
     </li>
