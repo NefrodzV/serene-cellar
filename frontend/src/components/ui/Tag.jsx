8bdc635486f3a03fa66dfd131f2b7897b1c0268f
Neftaly <neftalyrv23@gmail.com>
2025-10-10 18:40:41 -0400

    Added logic so tag can be selectable

--- frontend/src/components/ui/Tag.jsx
+++ frontend/src/components/ui/Tag.jsx
@@ -1,4 +1,19 @@
 import React from 'react'
-export function Tag({ children, ...props }) {
+import { Card } from './Card'
+export function Tag({ id, value, onChange, children }) {
+  if (onChange) {
+    return (
+      <Card
+        as="label"
+        variant="secondary"
+        className="selectable-card rounded"
+        htmlFor={id}
+      >
+        <input id={id} type="checkbox" value={value} onChange={onChange} />
+        <span className="checkmark"></span>
+        <span>{children}</span>
+      </Card>
+    )
+  }
   return <span>{children}</span>
 }
