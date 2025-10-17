5ba4aba8d8a2c9bf84221260c7acb66e1bf2610c
Neftaly <neftalyrv23@gmail.com>
2025-10-17 09:27:30 -0400

    Updated the query to get the cart items

--- backend/repositories/cart-repository.js
+++ backend/repositories/cart-repository.js
@@ -5,73 +5,27 @@ import { camelize } from '../utils/camelize.js'
 export async function getCartByUserId(userId) {
   const { rows } = await db.query(
     `
-    SELECT 
-    COALESCE(bool_and(item.purchasable), false) as can_checkout,
-    COALESCE(SUM(line_total), 0) as subtotal,
-    COALESCE(SUM(final_line_total), 0) as total,
-    COALESCE(json_agg(item), '[]') as items,
-    COALESCE(SUM(item.quantity), 0) as total_items,
-    CASE 
-      WHEN COALESCE(SUM(item.quantity), 0) > 0 THEN false
-      ELSE true
-    END as is_empty
-    FROM
-    (
+      SELECT 
+      COALESCE(bool_and(item.purchasable), false) as can_checkout,
+      COALESCE(SUM(item.quantity), 0) = 0 AS is_empty,
+      COALESCE(SUM(item.line_total),0) AS total,
+      COALESCE(SUM(item.quantity), 0) AS total_items,
+      COALESCE(json_agg(item), '[]'::json) AS items
+      FROM (
         SELECT 
-        p.name,
-        ci.id, 
-        ci.quantity, 
-        pr.value as price, 
-        pr.unit,
-        p.discount_percent,
-        pr.stock_quantity as stock,
-        (p.discount_percent <> 0 AND p.discount_percent IS NOT NULL) as has_discount,
-        CASE WHEN p.discount_percent <> 0
-          THEN ROUND(ci.quantity * (pr.value - (pr.value * p.discount_percent/100)), 2)
-          ELSE ci.quantity * pr.value
-        END as final_line_total,
-        (pr.value * ci.quantity) as line_total,
-        CASE
-          WHEN p.discount_percent IS NOT NULL 
-          THEN ROUND(pr.value - (pr.value * p.discount_percent / 100), 2)
-          ELSE pr.value
-        END as final_unit_price,
-        ARRAY_REMOVE(ARRAY[
-          CASE WHEN 
-          pr.stock_quantity < ci.quantity THEN 'INSUFFICIENT_STOCK' END,
-          CASE WHEN pr.stock_quantity = 0 THEN 'OUT_OF_STOCK' END,
-          CASE WHEN p.status <> 'active' THEN 'PRODUCT_UNAVAILABLE' END], null)
-        AS errors,
-        CASE
-          WHEN p.status <> 'active' THEN false
-          WHEN pr.stock_quantity = 0 THEN false
-          WHEN ci.quantity > COALESCE(pr.stock_quantity ,0) THEN false
-          ELSE true
-        END AS purchasable,
-        (
-          SELECT json_build_object(
-            'thumbnail', json_object_agg(
-              (regexp_match(pi.image_url, '([0-9]+)(?=\\.[A-Za-z0-9]+$)'))[1],
-              pi.image_url
-            ) FILTER (WHERE pi.image_url ILIKE '%thumb%'),
-             'gallery', json_object_agg(
-              (regexp_match(pi.image_url, '([0-9]+)(?=\\.[A-Za-z0-9]+$)'))[1],
-              pi.image_url
-            ) FILTER (WHERE pi.image_url NOT ILIKE '%thumb%')
-
-          )
-          FROM product_images pi
-          WHERE pi.product_id = p.id
-        ) AS images
+        ci.quantity,
+        pv.stock,
+        COALESCE(pv.stock, 0) > 0 AS purchasable,
+        ROUND(ci.quantity * p.amount, 2) as line_total
         FROM cart_items ci
-        INNER JOIN prices pr ON pr.id = ci.price_id
-        INNER JOIN products p ON p.id = pr.product_id
-        WHERE cart_id=(SELECT id FROM cart where user_id = $1)
+        INNER JOIN prices p ON p.id = ci.price_id
+        INNER JOIN product_variants pv ON pv.id = p.variant_id
+        WHERE cart_id = (SELECT id FROM carts WHERE user_id=$1)
+        
       ) item
-        `,
+    `,
     [userId]
   )
-
   return camelize(rows[0]) || null
 }
 
@@ -140,7 +94,7 @@ export async function getItemsByUserId(userId) {
 export async function createUserCart(userId) {
   await db.query(
     `
-        INSERT INTO cart
+        INSERT INTO carts
         (user_id) VALUES ($1)`,
     [userId]
   )
