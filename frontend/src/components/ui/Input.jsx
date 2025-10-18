74e84d29fefc917d26632548806614476bdd255b
Neftaly <neftalyrv23@gmail.com>
2025-10-18 16:37:17 -0400

    Made the input be in a wrapper

---
+++ frontend/src/components/ui/Input.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+export function Input({
+  variant = 'primary',
+  children,
+  id,
+  label,
+  error,
+  ...props
+}) {
+  const variants = {
+    primary: 'input-primary',
+  }
+  return (
+    <div className="input-container">
+      {label && <label htmlFor={id}>{label}</label>}
+      <div className="input-wrapper">
+        <input
+          className={`input ${variants[variant]}`}
+          id={id}
+          name={id}
+          aria-invalid={!!error}
+          aria-describedby={`error-${id}`}
+          {...props}
+        />
+        {children}
+      </div>
+      {error && (
+        <div className="error" id={`error-${id}`}>
+          {error}
+        </div>
+      )}
+    </div>
+  )
+}
