b24defacc5dcb623da5afd054d4e163fe9b8c8ba
Neftaly <neftalyrv23@gmail.com>
2025-10-25 21:28:55 -0400

    Romved the stagger

--- frontend/src/components/products/ProductCard.jsx
+++ frontend/src/components/products/ProductCard.jsx
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { Card } from '../ui'
 import { Link } from 'react-router-dom'
 import { ProductItem } from './ProductItem'
-export function ProductCard({ product, i, shouldShow = true }) {
+export function ProductCard({ product, i, shouldExit = false, ...props }) {
   const [entered, setEntered] = useState(false)
-  const [isLeaving, setIsLeaving] = useState(false)
-
+  // const delay = shouldExit ? 0 : i * 0.2
   useEffect(() => {
     requestAnimationFrame(() => requestAnimationFrame(() => setEntered(true)))
   }, [])
 
   return (
     <Card
-      key={product.id}
       as={Link}
       to={`/shop/${product?.id}`}
       variant="primary"
-      className={`rounded card-primary-hover from-left ${entered ? 'slide-in' : ''} `}
-      style={{ '--stagger': `${i * 0.2}s` }}
+      className={`rounded card-primary-hover from-left ${entered ? 'slide-in' : ''} ${shouldExit ? 'slide-out' : ''}`}
+      // style={{ '--stagger': `${delay}s` }}
+      {...props}
     >
       <ProductItem product={product} />
     </Card>
