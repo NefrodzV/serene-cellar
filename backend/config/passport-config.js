8bfa4d4d84eb8d55625ea32cd8051b36a42e55f4
Neftaly <neftalyrv23@gmail.com>
2025-11-12 18:18:08 -0400

    Fix issue with db not referencing pool

--- backend/config/passport-config.js
+++ backend/config/passport-config.js
@@ -1,6 +1,6 @@
 import passport from 'passport'
 import passportJwt from 'passport-jwt'
-import pool from '../db/pool.js'
+import { db } from '../db/index.js'
 import { configDotenv } from 'dotenv'
 configDotenv()
 
@@ -8,33 +8,33 @@ const JwtStrategy = passportJwt.Strategy
 const ExtractJwt = passportJwt.ExtractJwt
 
 passport.use(
-    new JwtStrategy(
-        {
-            jwtFromRequest: (req) => {
-                let token = null
-                if (req && req.cookies) {
-                    token = req.cookies['serene']
-                }
-                return token
-            },
-            secretOrKey: process.env.JWT_SECRET,
-        },
-        async (jwt_payload, done) => {
-            try {
-                const { userId } = jwt_payload
-                const { rows } = await pool.query(
-                    'SELECT * FROM users WHERE id=$1',
-                    [userId]
-                )
-                if (rows.length === 0) return done(null, false)
-                const user = rows[0]
-                return done(null, user)
-            } catch (error) {
-                console.error('Database query error', error)
-                return done(error, false)
-            }
+  new JwtStrategy(
+    {
+      jwtFromRequest: (req) => {
+        let token = null
+        if (req && req.cookies) {
+          token = req.cookies['serene']
         }
-    )
+        return token
+      },
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (jwt_payload, done) => {
+      try {
+        const { userId } = jwt_payload
+        const { rows } = await db.pool.query(
+          'SELECT * FROM users WHERE id=$1',
+          [userId]
+        )
+        if (rows.length === 0) return done(null, false)
+        const user = rows[0]
+        return done(null, user)
+      } catch (error) {
+        console.error('Database query error', error)
+        return done(error, false)
+      }
+    }
+  )
 )
 
 export default passport
