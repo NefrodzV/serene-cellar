73066ddcf4cc361670b70308633223f5d8bb0557
Neftaly <neftalyrv23@gmail.com>
2025-11-28 18:26:40 -0400

    Rename field to webhook secret and added the logic to create user order

--- backend/controllers/checkout-controller.js
+++ backend/controllers/checkout-controller.js
@@ -4,9 +4,14 @@ import express from 'express'
 import { userRepository } from '../repositories/user-repository.js'
 import { configDotenv } from 'dotenv'
 import { getItemsForStripe } from '../repositories/cart-repository.js'
+import {
+  createOrder,
+  createOrderItem,
+} from '../repositories/order-repository.js'
+import { withTransaction } from '../db/pool.js'
 configDotenv()
 
-const endpointSecret = process.env.WEBHOOK_SECRET
+const webhookSecret = process.env.WEBHOOK_SECRET
 const frontendDomain = process.env.FRONTEND_DOMAIN
 const stripeSecret = process.env.STRIPE_SECRET
 
@@ -29,6 +34,7 @@ export const createSession = [
     }
 
     const cartItems = await getItemsForStripe(req.user.id)
+
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
       line_items: cartItems.map((item) => ({
@@ -38,12 +44,18 @@ export const createSession = [
           unit_amount: Math.round(item.amount * 100), // calculate price from numeric to cents
           product_data: {
             name: `${item.name} (${item.displayName})`,
+            metadata: {
+              variant_id: item.variantId,
+            },
           },
         },
       })),
       mode: 'payment',
       success_url: `${frontendDomain}/checkout/success?sessionId={CHECKOUT_SESSION_ID}`,
       cancel_url: `${frontendDomain}/checkout/cancel`,
+      metadata: {
+        user_id: req.user.id,
+      },
     })
 
     return res.json({ url: session.url })
@@ -51,16 +63,38 @@ export const createSession = [
 ]
 export const hook = [
   express.raw({ type: 'application/json' }),
-  (req, res) => {
+  async (req, res) => {
     const sig = req.headers['stripe-signature']
     let event
     try {
-      event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret)
+      event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret)
+
+      if (event.type === 'checkout.session.completed') {
+        const session = event.data.object
+        const lineItems = await stripe.checkout.sessions.listLineItems(
+          session.id,
+          {
+            expand: ['data.price.product'],
+          }
+        )
+        const items = lineItems.data.map((item) => ({
+          variantId: parseInt(item.price.product.metadata.variant_id, 10),
+          quantity: item.quantity,
+          price: (item.price.unit_amount / 100).toFixed(2), // Calculate numeric price from cents
+        }))
+        const userId = parseInt(session.metadata.user_id, 10)
+        await withTransaction(async (client) => {
+          const order = await createOrder(client, userId, session.id)
+          for (const item of items) {
+            await createOrderItem(client, order.id, item)
+          }
+        })
+      }
     } catch (err) {
-      console.log('Stripe hook error:', hook)
+      console.log('Stripe hook error:', err)
       return res.sendStatus(400)
     }
-    console.log('Event type:', event.type)
+    // console.log('Event type:', event.type)
     return res.sendStatus(200)
   },
 ]
