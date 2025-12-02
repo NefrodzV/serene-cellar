91ec21eaf2f7773d6ed1e29b86ed31019996e46c
Neftaly <neftalyrv23@gmail.com>
2025-12-02 18:01:14 -0400

    made the elements for the profile page and order section. Also styled them.

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -4,7 +4,77 @@ import React, { useEffect, useState } from 'react'
 API_URL
 export function ProfilePage() {
   const { user } = useUser()
-  const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState([
+    {
+      id: 1,
+      date: '2025/25/12',
+      items: [
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+      ],
+      boughtItems: 3,
+      total: 20.75,
+    },
+    {
+      id: 2,
+      date: '2025/25/12',
+      items: [
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+      ],
+      boughtItems: 3,
+      total: 20.75,
+    },
+    {
+      id: 3,
+      date: '2025/25/12',
+      items: [
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+        {
+          name: 'item name 1',
+          quantity: 2,
+          price: 8.12,
+        },
+      ],
+      boughtItems: 3,
+      total: 20.75,
+    },
+  ])
 
   useEffect(() => {
     async function getOrders() {
@@ -27,16 +97,56 @@ export function ProfilePage() {
 
   return (
     <div className="profile-page">
-      <section className="user-section">
-        <img
-          className="user-image"
-          src={user?.image || <i class="fa-solid fa-user"></i>}
-          alt="User profile picture"
-        />
-        <span>{user?.name}||user name placeholder</span>
+      <h1>Profile</h1>
+      <section className="profile-user">
+        <span className="user-image-wrapper">
+          <i className="fa-solid fa-user"></i>
+        </span>
+        <div className="profile-user-details">
+          <div className="profile-username">
+            {user?.name}||user name placeholder
+          </div>
+          <div>user email</div>
+        </div>
       </section>
       <section className="orders">
-        <ul className="order-list"></ul>
+        <h2>Order History</h2>
+        <ul className="order-list">
+          {orders.map((order) => (
+            <li key={order.id} className="order-item">
+              <div className="order-item-header">
+                <span className="order-item-date">Date: {order.date}</span>
+                <span className="order-item-total-items">
+                  Items bought: {order.boughtItems}
+                </span>
+                <span className="order-item-total">
+                  Total spent: $ {order.total}
+                </span>
+              </div>
+              <details>
+                <summary>Items bought</summary>
+                <ul className="bought-items-list">
+                  {order.items.map((item) => (
+                    <li className="bought-item">
+                      <div className="bought-item-image-container">
+                        {item.imageUrl ? (
+                          <img src={item?.imageUrl} alt="Bought item image" />
+                        ) : (
+                          <i className="bought-item-image-placeholder fa-regular fa-image"></i>
+                        )}
+                      </div>
+                      <div className="bought-item-details">
+                        <span>{item.name}</span>
+                        <span>Bought: {item.quantity}</span>
+                        <span>Price: $ {item.price} ea</span>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </details>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   )
