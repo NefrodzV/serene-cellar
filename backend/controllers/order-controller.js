bf86709d1ca3ed23765e4f79681b23539c2ef5ef
Neftaly <neftalyrv23@gmail.com>
2025-12-04 18:28:22 -0400

    remove log

--- backend/controllers/order-controller.js
+++ backend/controllers/order-controller.js
@@ -4,11 +4,9 @@ import { getOrdersByUserId } from '../repositories/order-repository.js'
 export const getOrders = [
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
-    console.log('running')
     try {
       const userId = req.user.id
       const orders = await getOrdersByUserId(userId)
-      console.log(orders)
       return res.json({ orders })
     } catch (error) {
       console.error(error)
