1d5f91bc09222a6c4bfd10f8d5e52f7d7fe09a5f
Neftaly <neftalyrv23@gmail.com>
2026-01-03 15:45:58 -0400

    Changing port for production

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import { errorHandler } from './middlewares/errorHandler.js'
 import { checkoutController } from './controllers/index.js'
 2
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const corsOptions = {
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
