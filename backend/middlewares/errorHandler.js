9d6d4144772e205bbbe36c18026d9068b5f6f697
Neftaly <neftalyrv23@gmail.com>
2026-01-06 14:49:34 -0400

    Loggin error handler and seeing if its running

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,6 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack)
-    return res.status(500).json({
-        message: 'An unexpected error happened when processing the request.',
-    })
+  console.log('error handler running')
+  console.error(err.stack)
+  return res.status(500).json({
+    message: 'An unexpected error happened when processing the request.',
+  })
 }
