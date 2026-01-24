fc4217047f0eee6ec5c45c7d6882444fb7874335
Neftaly <neftalyrv23@gmail.com>
2026-01-24 19:00:44 -0400

    Added a new comment for error handling

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -3,14 +3,15 @@ export const errorHandler = (err, req, res, next) => {
   const isSleeping =
     code === 'ETIMEDOUT' || 'ECONNREFUSED' || err?.name === 'AggregateError'
 
-  console.log('Sleeping is running value is:', isSleeping)
   if (isSleeping) {
     console.log('Sleeping sending 503')
     return res.sendStatus(503)
   }
 
   if (res.headerSent) return next(err)
-  console.error('Unexpected error happened', err.stack)
+
+  console.error('Some error happened: ', err)
+  console.error('Error stack: ', err.stack)
   return res.status(err.status || 500).json({
     message: 'An unexpected error happened when processing the request.',
   })
