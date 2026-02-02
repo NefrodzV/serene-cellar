b1b98bd9171d727b762558462a18bb65a43f6be3
Neftaly <neftalyrv23@gmail.com>
2026-02-02 11:07:58 -0400

    Change the code dependency in thumbnail component

--- frontend/src/components/products/ProductItem.jsx
+++ frontend/src/components/products/ProductItem.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Title, Thumbnail, PriceTag } from '../ui'
+import { API_URL } from '../../config'
+import { ProductTitle } from '../product/ProductTitle'
+
 export function ProductItem({ product }) {
-  const { images, name, variants } = product
-  const smallestPrice = variants[0].price.toString()
+  const smallestPrice = product?.variants[0].price.toString()
   const [dollars = 0, cents = 0] = smallestPrice?.split('.')
   return (
-    <article className="product-item" state={{ product }}>
-      <Thumbnail data={images?.thumbnail} />
-      <Title>{name}</Title>
+    <article className="product-item">
+      <Thumbnail
+        images={{
+          150: `${API_URL}/${product?.images?.thumbnail?.[150]}`,
+          300: `${API_URL}/${product?.images?.thumbnail?.[300]}`,
+          450: `${API_URL}/${product?.images?.thumbnail?.[450]}`,
+        }}
+        alt={`${product.name} thumbnail`}
+      />
+      <ProductTitle text={product?.name} />
       <PriceTag dollars={dollars} cents={cents} />
     </article>
   )
--- frontend/src/components/ui/Thumbnail.jsx
+++ frontend/src/components/ui/Thumbnail.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { API_URL } from '../../config'
-export function Thumbnail({ data }) {
+export function Thumbnail({ images, alt }) {
   return (
     <img
       className="thumbnail"
-      src={`${API_URL}/${data[150]}`}
-      srcSet={`${API_URL}/${data[150]} 1x, ${API_URL}/${data[300]} 2x, ${API_URL}/${data[450]} 3x`}
+      src={images[300]}
+      srcSet={`${images[150]} 150w, ${images[300]} 300w, ${images[450]} 450w`}
+      alt={alt}
     />
   )
 }
