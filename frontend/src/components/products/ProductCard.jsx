28c19564a37012868e0eb87dd35476180f96bb21
Neftaly <neftalyrv23@gmail.com>
2026-02-06 19:30:43 -0400

    Update with decoupled

--- frontend/src/components/products/ProductCard.jsx
+++ frontend/src/components/products/ProductCard.jsx
@@ -3,15 +3,13 @@ import { Card } from '../ui'
 import { Link } from 'react-router-dom'
 import { ProductItem } from './ProductItem'
 export function ProductCard({ product, i, shouldExit = false, ...props }) {
-  const [entered, setEntered] = useState(false)
-  // const delay = shouldExit ? 0 : i * 0.2
-  useEffect(() => {
-    requestAnimationFrame(() => requestAnimationFrame(() => setEntered(true)))
-  }, [])
-
   return (
     <Card as={Link} to={`/shop/${product?.id}`} {...props}>
-      <ProductItem product={product} />
+      <ProductItem
+        productName={product?.name}
+        price={product?.variants?.[0].price}
+        images={product?.images?.thumbnail}
+      />
     </Card>
   )
 }
