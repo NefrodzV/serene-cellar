29dd10d7115bf042724af9dfbb0ac49e2ef617ad
Neftaly <neftalyrv23@gmail.com>
2026-02-13 15:20:03 -0400

    Increased the default text size and changed the subttoal in product page

--- frontend/src/components/product/ProductSubtotal.jsx
+++ frontend/src/components/product/ProductSubtotal.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { ProductPrice } from './ProductPrice'
 export function ProductSubtotal({ subtotal }) {
-  return <div className="product-subtotal">Subtotal: ${subtotal}</div>
+    return (
+        <div className="product-subtotal">
+            <div className="subtotal-label">Subtotal</div>
+            <ProductPrice price={subtotal} />
+        </div>
+    )
 }
