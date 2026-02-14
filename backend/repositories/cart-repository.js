236501bc44a5f92c65d48ab2956f1feb30a67017
Neftaly <neftalyrv23@gmail.com>
2026-02-14 17:38:06 -0400

    Added unquie constraint to create cart function for unique cart items per cart.

--- backend/repositories/cart-repository.js
+++ backend/repositories/cart-repository.js
@@ -2,8 +2,8 @@ import { pool } from '../db/pool.js'
 import { camelize } from '../utils/camelize.js'
 
 export async function getCartByUserId(userId) {
-  const { rows } = await pool.query(
-    `
+    const { rows } = await pool.query(
+        `
       SELECT 
       COALESCE(bool_and(item.purchasable), false) as can_checkout,
       COALESCE(SUM(item.quantity), 0) = 0 AS is_empty,
@@ -17,6 +17,7 @@ export async function getCartByUserId(userId) {
         ci.quantity,
         pv.stock,
         p.amount AS price,
+        pkg.quantity_per_package,
         pkg.display_name as package,
         c.ml,
         c.kind as container,
@@ -48,66 +49,66 @@ export async function getCartByUserId(userId) {
         WHERE cart_id = (SELECT id FROM carts WHERE user_id=$1)
       ) item 
     `,
-    [userId]
-  )
-  return camelize(rows[0]) || null
+        [userId]
+    )
+    return camelize(rows[0]) || null
 }
 
 export async function setCartItemQuantity(itemId, quantity) {
-  await pool.query(
-    `
+    await pool.query(
+        `
       UPDATE cart_items
       SET quantity=$1
       WHERE id=$2`,
-    [quantity, itemId]
-  )
+        [quantity, itemId]
+    )
 }
 export async function incrementCartItemQuantity(
-  itemId,
-  quantity,
-  client = pool
+    itemId,
+    quantity,
+    client = pool
 ) {
-  await client.query(
-    `
+    await client.query(
+        `
         UPDATE cart_items
         SET quantity = GREATEST(quantity + $1, 0)
         WHERE id=$2
         `,
-    [quantity, itemId]
-  )
+        [quantity, itemId]
+    )
 }
 
 export async function getCartItemByPriceId(userId, priceId) {
-  const { rows } = await pool.query(
-    `
+    const { rows } = await pool.query(
+        `
         SELECT id FROM cart_items
         WHERE price_id=$1 AND
         cart_id=(SELECT id from carts WHERE user_id=$2)`,
-    [priceId, userId]
-  )
-  return rows[0] || null
+        [priceId, userId]
+    )
+    return rows[0] || null
 }
 
 export async function createCartItem(userId, quantity, priceId, client = pool) {
-  await client.query(
-    `INSERT INTO cart_items 
+    await client.query(
+        `INSERT INTO cart_items 
         (cart_id, quantity, price_id)
-        VALUES ((SELECT id from carts WHERE user_id=$1), $2, $3) ON CONFLICT(price_id) DO UPDATE SET quantity = EXCLUDED.quantity + cart_items.quantity`,
-    [userId, quantity, priceId]
-  )
+        VALUES ((SELECT id from carts WHERE user_id=$1), $2, $3) ON CONFLICT(cart_id, price_id) DO UPDATE SET quantity = EXCLUDED.quantity + cart_items.quantity`,
+        [userId, quantity, priceId]
+    )
 }
 
 export async function deleteCartItem(itemId) {
-  await pool.query(
-    `DELETE FROM cart_items 
+    await pool.query(
+        `DELETE FROM cart_items 
         WHERE id=$1`,
-    [itemId]
-  )
+        [itemId]
+    )
 }
 
 export async function getItemsByUserId(userId) {
-  const { rows } = await pool.query(
-    `
+    const { rows } = await pool.query(
+        `
         SELECT 
         pv.product_id,
         ci.price_id 
@@ -115,34 +116,34 @@ export async function getItemsByUserId(userId) {
         INNER JOIN prices p ON p.id= ci.price_id
         INNER JOIN product_variants pv ON pv.id = p.variant_id
         WHERE cart_id=(SELECT id from carts WHERE user_id=$1)`,
-    [userId]
-  )
+        [userId]
+    )
 
-  return rows
+    return rows
 }
 
 export async function createUserCart(userId) {
-  await pool.query(
-    `
+    await pool.query(
+        `
         INSERT INTO carts
         (user_id) VALUES ($1)`,
-    [userId]
-  )
+        [userId]
+    )
 }
 
 export async function validateLocalCartItems(items) {
-  console.log('this is getting called')
-  const values = items
-    .map(
-      (_, i) =>
-        `(CAST($${i * 3 + 1} as uuid),CAST($${i * 3 + 2} AS int),CAST($${i * 3 + 3} AS int))`
-    )
-    .join(',')
-  const params = items
-    .map((item) => [item.id, Number(item.priceId), Number(item.quantity)])
-    .flat()
-  const { rows } = await pool.query(
-    `
+    console.log('this is getting called')
+    const values = items
+        .map(
+            (_, i) =>
+                `(CAST($${i * 3 + 1} as uuid),CAST($${i * 3 + 2} AS int),CAST($${i * 3 + 3} AS int))`
+        )
+        .join(',')
+    const params = items
+        .map((item) => [item.id, Number(item.priceId), Number(item.quantity)])
+        .flat()
+    const { rows } = await pool.query(
+        `
       SELECT 
       COALESCE(bool_and(item.purchasable), false) AS can_checkout,
       COALESCE(SUM(item.quantity), 0) = 0 AS is_empty,
@@ -156,7 +157,7 @@ export async function validateLocalCartItems(items) {
         lc.price_id,
         pr.name,
         lc.quantity,
-        pkg.display_name as package,
+        pkg.quantity_per_package,
         c.kind as container, 
         c.ml,
         ROUND(lc.quantity * p.amount, 2) AS line_total,
@@ -188,16 +189,16 @@ export async function validateLocalCartItems(items) {
         INNER JOIN containers c ON c.id = pv.container_id
       ) item
     `,
-    params
-  )
+        params
+    )
 
-  console.log(rows[0].items)
-  return camelize(rows[0]) || null
+    console.log(rows[0].items)
+    return camelize(rows[0]) || null
 }
 
 export async function getItemsForStripe(userId) {
-  const { rows } = await pool.query(
-    `
+    const { rows } = await pool.query(
+        `
       SELECT
         pv.id as variant_id,
         pr.amount, 
@@ -211,8 +212,8 @@ export async function getItemsForStripe(userId) {
       INNER JOIN products prod ON prod.id = pv.product_id
       WHERE cart_id=(SELECT id from carts WHERE user_id=$1)
     `,
-    [userId]
-  )
+        [userId]
+    )
 
-  return camelize(rows)
+    return camelize(rows)
 }
