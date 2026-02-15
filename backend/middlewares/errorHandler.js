2038651a02cd35f2de3af7817d2320f58c3c1bdc
Neftaly <neftalyrv23@gmail.com>
2026-02-15 15:18:48 -0400

    Updated the error handler to be more specific.

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,19 +1,23 @@
 export const errorHandler = (err, req, res, next) => {
-  const code = err?.code
-  const isSleeping =
-    code === 'ETIMEDOUT' || 'ECONNREFUSED' || err?.name === 'AggregateError'
+    const code = err?.code
+    const isSleeping =
+        code === 'ETIMEDOUT' || 'ECONNREFUSED' || err?.name === 'AggregateError'
 
-  if (isSleeping) {
-    console.log('Sleeping sending 503')
-    console.error(err)
-    return res.sendStatus(503)
-  }
+    if (isSleeping) {
+        console.log('Sleeping sending 503')
+        console.log('Error message')
+        console.error(err.message)
 
-  if (res.headerSent) return next(err)
+        console.log('Error stack', err.stack)
 
-  console.error('Some error happened: ', err)
-  console.error('Error stack: ', err.stack)
-  return res.status(err.status || 500).json({
-    message: 'An unexpected error happened when processing the request.',
-  })
+        return res.sendStatus(503)
+    }
+
+    if (res.headerSent) return next(err)
+
+    console.error('Some error happened: ', err.message)
+    console.error('Error stack: ', err.stack)
+    return res.status(err.status || 500).json({
+        message: 'An unexpected error happened when processing the request.',
+    })
 }
