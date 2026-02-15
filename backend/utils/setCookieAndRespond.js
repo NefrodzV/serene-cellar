a6cf13efdcc31ccd6716e67bfce402031bbc0197
Neftaly <neftalyrv23@gmail.com>
2026-02-15 16:26:25 -0400

    Upload fix issue with cookies  with different domains, since backend and frontend domain are different they get blocked in production.

--- backend/utils/setCookieAndRespond.js
+++ backend/utils/setCookieAndRespond.js
@@ -1,8 +1,15 @@
+import { configDotenv } from 'dotenv'
+
+configDotenv()
 export function setCookieAndRespond(req, res) {
     const { token, user } = req
+    const isProduction = process.env.NODE_ENV === 'production'
     res.cookie('serene', token, {
         maxAge: 1000 * 60 * 60 * 24 * 7, // Last 7 days,
         httpOnly: true,
+        sameSite: isProduction ? 'none' : 'lax',
+        secure: isProduction,
+        path: '/',
     })
 
     return res.status(201).json({
